b7a74e4a6360d1f83945057b040239632864a6ec
Victor Chen <60045243+wizardassassin@users.noreply.github.com>
2022-11-15 14:37:00 -0800

    Added Item names to auctions

--- app/skyblock/auctions/page.tsx
+++ app/skyblock/auctions/page.tsx
@@ -1,5 +1,6 @@
 import { getAuctions, AItemObj } from "utils/auctions";
 import { getTranslations, TItemObj } from "utils/translations";
+import { toTitleCase } from "utils/utils";
 
 export default async function AuctionsPage() {
   const dataP = getAuctions();
@@ -29,6 +30,21 @@ export default async function AuctionsPage() {
 
 function Item2({ data, name }: { data: AItemObj; name: TItemObj | undefined }) {
   let displayName = name?.name;
+  const key = ["Common", "Uncommon", "Rare", "Epic", "Legendary", "Mythic"];
+  if (data.productID.includes(";")) {
+    const tmpArr2 = data.productID.split(";");
+    const num = tmpArr2[1];
+    const tmpArr = tmpArr2[0].split("_");
+    const rarity = num ? " - " + (key[Number(num)] ?? num) : "";
+    displayName = tmpArr.map((x) => toTitleCase(x)).join(" ") + rarity;
+  } else if (!displayName) {
+    // Hotfix for now
+    const tmpArr1 = data.productID.split("-");
+    const num = tmpArr1[1];
+    const tmpArr2 = tmpArr1[0].split("_");
+    const rarity = num ? " - " + (key[Number(num)] ?? num) : "";
+    displayName = tmpArr2.map((x) => toTitleCase(x)).join(" ") + rarity;
+  }
   displayName ??= data.productID;
   if (displayName === data.productID) {
     console.log(data.productID);
