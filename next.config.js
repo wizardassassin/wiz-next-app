c9789ce54d0d6ab0228de6865f9d462707f3cf90
Victor <60045243+wizardassassin@users.noreply.github.com>
2024-12-25 23:19:21 +0000

    Updated Packages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: { appDir: true },
+  // swcMinify: true,
+  // experimental: { appDir: true },
 };
 
 module.exports = nextConfig;
